0ac410a6cdd573b0da8a8de361707b2af50c1d9c
Hiroshi Ito <itohiro73@gmail.com>
2017-11-21 23:04:06 +0900

    Add crontab config to run provisioning every Monday morning.

--- roles/profile/templates/provision.sh
+++ roles/profile/templates/provision.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/zsh
 
-cd $PROVISION_HOME
+export PATH=$PATH:/usr/local/bin
+
+cd /Users/hiroshi/.provisioning
 ansible-playbook -i hosts -vv dev-env-setup.yml
 source ~/.zshenv
 source ~/.zshrc
